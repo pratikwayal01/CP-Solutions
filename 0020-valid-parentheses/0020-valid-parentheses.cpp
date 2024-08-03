b09ac4272dedced9033b53ccd89806d94e991b49
Pratik wayal <97722446+pratikwayal01@users.noreply.github.com>
2024-08-03 17:52:10 +0530

    Time: 4 ms (11.97%), Space: 7.8 MB (37.00%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> stk;
+    for(int i = 0; i < s.size(); i++) {
+        if (s[i] == '(' || s[i] == '{' || s[i] == '[') {
+            stk.push(s[i]);
+        } else {
+            if (stk.empty()) return false;
+            if ((s[i] == ')' && stk.top() == '(') ||
+                (s[i] == '}' && stk.top() == '{') ||
+                (s[i] == ']' && stk.top() == '[')) {
+                stk.pop();
+            } else {
+                return false;
+            }
+        }
+    }
+    return stk.empty();
+    }
+};
